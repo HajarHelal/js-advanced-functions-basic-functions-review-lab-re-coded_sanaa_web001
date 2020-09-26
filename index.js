c9bd4ed239d8698012fd0ef71e43d68867ab8aeb
Hajar Helal <hajerhelal44@gmail.com>
2020-09-26 23:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const Calculator = {
   }
 }
 
-let actionApplyer = function(start, array) {
+ function actionApplyer(start, array) {
   let a = start
   for (let i = 0; i < array.length; i++ ){
     a = array[i](a)
